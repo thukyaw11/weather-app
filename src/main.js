7450ba6fc713111aa85a0bd413c6af4695bb989a
thukyaw11 <helloeleven968@gmail.com>
2020-01-31 12:43:39 +0630

    ui updated

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Button from 'ant-design-vue/lib/button';
+import Input from 'ant-design-vue/lib/input';
+import 'ant-design-vue/dist/antd.css';
 Vue.config.productionTip = false
-
+Vue.use(Button);
+Vue.use(Input);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
