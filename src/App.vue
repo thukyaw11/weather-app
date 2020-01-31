0bc401d5be01062c5ee18403b604ea47b05bca2d
thukyaw11 <helloeleven968@gmail.com>
2020-01-31 20:57:10 +0630

    ui updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/home" class="nav-item"><a-icon type="search" class="icon"/></router-link> 
+      <router-link to="/" class="nav-item"><a-icon type="search" class="icon"/></router-link> 
       <router-link to="/save" class="nav-item-lg"><a-icon type="plus" class="icon-lg"/></router-link> 
       <router-link to="/about" class="nav-item"><a-icon type="question" class="icon"/></router-link>
     </div>
